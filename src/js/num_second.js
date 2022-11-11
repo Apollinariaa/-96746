524dba82c66610c995f7f46ac7beae27f4b750f8
Полина <2002polina2016@gmail.com>
2022-11-11 14:31:47 +0300

    edit task num2

--- src/js/num_second.js
+++ src/js/num_second.js
@@ -1,20 +1,13 @@
 /*jshint esversion: 8 */
 function arrUniqueElements(arr) {
-	const arrObjects = [];
-	arr.filter((item, i, ar) => ar.indexOf(item) === i).sort().map((uniqueElem) => {
-		objct = { 
+	return arr.filter((item, i, ar) => ar.indexOf(item) === i).map((uniqueElem) => ({
 			name: uniqueElem, 
-			qty: arr.reduce((count,item) => { (uniqueElem === item) ? count++: count; return count; },0)
-		};
-		arrObjects.push(objct);
-	});
-	console.log(arrObjects);
-	return arrObjects.sort((a, b) => a.qty > b.qty ? -1 : 1).map(item => { return item.name;});
+			qty: arr.reduce((count,item) => { 
+				if (uniqueElem === item) count++;
+				return count; 
+			},0) 
+		})).sort((a, b) => a.qty > b.qty ? -1 : 1).map(item => { return item.name;});
 }
-
-const result = arrUniqueElements(["c", "b", "a","b","c","c","a" ]);
+const result = arrUniqueElements(["c", "b", "a", "b", "c", "c", "a" ]);
 console.log(result);
-
-//[ { name: 'a', qty: 2 }, { name: 'b', qty: 2 }, { name: 'c', qty: 3 } ]
-//[ 'c', 'a', 'b' ]
 
