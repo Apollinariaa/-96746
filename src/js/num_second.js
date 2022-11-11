ef83cb7bce6e4973a5f150573fed9f7c83026522
Полина <2002polina2016@gmail.com>
2022-11-11 13:21:06 +0300

    edit task num2

--- src/js/num_second.js
+++ src/js/num_second.js
@@ -1,23 +1,20 @@
 /*jshint esversion: 8 */
 function arrUniqueElements(arr) {
-	const uniqueArr = arr.filter((item, i, ar) => ar.indexOf(item) === i);
-	const arrObject = [];
-	for (let i=0; i< uniqueArr.length; i++) {
-		let count = 0;
-		for (let j=0; j<arr.length; j++) {
-			if (uniqueArr[i] === arr[j]){
-				count +=1;
-			}
-		}
-		objct = {
-			name: uniqueArr[i],
-			qty: count
+	const arrObjects = [];
+	arr.filter((item, i, ar) => ar.indexOf(item) === i).sort().map((uniqueElem) => {
+		objct = { 
+			name: uniqueElem, 
+			qty: arr.reduce((count,item) => { (uniqueElem === item) ? count++: count; return count; },0)
 		};
-		arrObject.push(objct);
-	}
-	arrObject.sort((a, b) => a.qty > b.qty ? -1 : 1);
-	return arrObject.map(item => { return item.name;});
+		arrObjects.push(objct);
+	});
+	console.log(arrObjects);
+	return arrObjects.sort((a, b) => a.qty > b.qty ? -1 : 1).map(item => { return item.name;});
 }
 
-const result = arrUniqueElements(['keyboard', 'banana','keyboard','fruit','banana', 'keyboard', 'banana']);
+const result = arrUniqueElements(["c", "b", "a","b","c","c","a" ]);
 console.log(result);
+
+//[ { name: 'a', qty: 2 }, { name: 'b', qty: 2 }, { name: 'c', qty: 3 } ]
+//[ 'c', 'a', 'b' ]
+
